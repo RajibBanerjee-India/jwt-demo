bcbc6100bf2946a56a4b8d417cd2a0a94b1ae878
Rajib Banerjee <rb021990@gmail.com>
2025-05-01 22:52:03 -0400

    Authentication Controller

---
+++ src/main/java/ca/rajib/jwt_demo/controller/AuthController.java
@@ -0,0 +1,61 @@
+package ca.rajib.jwt_demo.controller;
+
+import ca.rajib.jwt_demo.model.User;
+import ca.rajib.jwt_demo.repository.UserRepository;
+import ca.rajib.jwt_demo.security.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth/")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTUtil jwtUtil;
+
+    @Autowired
+    AuthController(AuthenticationManager authenticationManager,
+                   UserRepository userRepository,
+                   PasswordEncoder passwordEncoder,
+                   JWTUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/signin")
+    public String authenticateUser(@RequestBody User user) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(
+                        user.getUsername(), user.getPassword()
+                ));
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return jwtUtil.generateToken(userDetails.getUsername());
+    }
+
+    @PostMapping("/signup")
+    public String registerUser(@RequestBody User user) {
+        if (userRepository.existsByUsername(user.getUsername())) {
+            return "Error! username already taken";
+        }
+
+        // Create new user's account
+        User newUser = new User(null, user.getUsername(),
+                passwordEncoder.encode(user.getPassword()));
+
+        userRepository.save(newUser);
+        return "User registered successfully!";
+    }
+}
